8220f073704c109da1cede4540bf5dabc05d0b82
rendyputrak <rendyputrak@gmail.com>
2024-02-23 10:24:36 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/hello', function () {
+    return 'Hello World';
+    });
+
+Route::get('/world', function () {
+    return 'World';
+    });
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+    });
+
+Route::get('/about', function ($nim = '2241720124', 
+                                $name = 'Rendy Putra Kusuma') {
+    return 'Nama saya adalah ' . $name . 
+    ' dan NIM saya adalah ' . $nim;
 });
+
+// Route::get('/about/{name}/{nim}', function ($name, $nim) {
+//     return 'Nama saya adalah ' . $name . ' dan NIM saya adalah ' . $nim;
+// });
+
+Route::get('/user/{name}', function ($name){
+    return 'Nama saya adalah ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', 
+            function ($postId, $commentId) { 
+    return 'Pos ke-' . $postId." Komentar ke-: " . $commentId; 
+}); 
+
+Route::get('/articles/{id}', function ($id){
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name='John'){
+    return 'Nama saya ' . $name;
+});
+
+// Route::get('/user/profile', function() {
+// //
+// })->name('profile'); 
+
+
