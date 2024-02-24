ed7131565a24aa3da328bf9dfe4e994a55fc2853
rendyputrak <rendyputrak@gmail.com>
2024-02-24 19:29:28 +0700

    Controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,15 +6,15 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    // public function index(){
-    //     return 'Selamat Datang';
-    // }
-    // public function about($nim = '2241720124', 
-    // $name = 'Rendy Putra Kusuma'){
-    //     return 'Nama saya adalah ' . $name . 
-    //     ' dan NIM saya adalah ' . $nim;
-    // }
-    // public function articles($id=null){
-    //     return 'Halaman Artikel dengan ID ' . $id;
-    // }
+    public function index(){
+        return 'Selamat Datang';
+    }
+    public function about($nim = '2241720124', 
+    $name = 'Rendy Putra Kusuma'){
+        return 'Nama saya adalah ' . $name . 
+        ' dan NIM saya adalah ' . $nim;
+    }
+    public function articles($id=null){
+        return 'Halaman Artikel dengan ID ' . $id;
+    }
 }
